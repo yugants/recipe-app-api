779e6f420244b7c5f6f2f3fd0d9a6e081c9d44ce
Yugant Shekhar <yugantshekhar@gmail.com>
2022-08-01 11:10:07 +0530

    Correcting Linting error, Postgres configured

--- app/app/tests.py
+++ app/app/tests.py
@@ -16,6 +16,6 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
-        '''Test substracting numbers''' 
+        '''Test substracting numbers'''
         res = calc.substract(10, 15)
         self.assertEqual(res, 5)
