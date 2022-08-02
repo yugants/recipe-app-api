39ebfc55e35674c76e59568ac94e981c703f9f23
Yugant Shekhar <yugantshekhar@gmail.com>
2022-08-02 15:14:02 +0530

    With custom User model

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,4 +130,6 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'core.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
