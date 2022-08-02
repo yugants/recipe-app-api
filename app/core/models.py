28a181857bc9815436ad33baa934dfedaabe6587
Yugant Shekhar <yugantshekhar@gmail.com>
2022-08-02 15:20:53 +0530

    User models setup

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
@@ -29,6 +30,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
